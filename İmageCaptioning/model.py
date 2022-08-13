2f431deb48075fc48c966006e4a0f48309a345f8
enes3774 <77508537+enes3774@users.noreply.github.com>
2022-08-13 22:44:32 +0300

    Create model.py

---
+++ İmageCaptioning/model.py
@@ -0,0 +1,32 @@
+from transformers import AutoModel, AutoTokenizer,AutoModelWithLMHead
+
+class ClipCaptionModel(nn.Module):
+
+    def get_dummy_token(self, batch_size: int, device: torch.device) -> torch.Tensor:
+        return torch.zeros(batch_size, self.prefix_length, dtype=torch.int64, device=device)
+
+    def forward(self, tokens: torch.Tensor, prefix: torch.Tensor, mask: Optional[torch.Tensor] = None,
+                labels: Optional[torch.Tensor] = None):
+        embedding_text = self.text_model.transformer.wte(tokens)
+        
+        prefix_projections = self.clip_project(prefix).view(-1, self.prefix_length, self.text_model_embedding_size)
+        embedding_cat = torch.cat((prefix_projections, embedding_text), dim=1)
+        if labels is not None:
+            dummy_token = self.get_dummy_token(tokens.shape[0], tokens.device)
+            labels = torch.cat((dummy_token, tokens), dim=1)
+        out = self.text_model(inputs_embeds=embedding_cat, labels=labels, attention_mask=mask)
+        return out
+
+    def __init__(self, prefix_length: int, clip_length: Optional[int] = None, prefix_size: int = 512,
+                 num_layers: int = 8, mapping_type: MappingType = MappingType.MLP):
+        super(ClipCaptionModel, self).__init__()
+        self.prefix_length = prefix_length
+        self.text_model = AutoModelWithLMHead.from_pretrained("redrussianarmy/gpt2-turkish-cased")
+       
+        self.text_model_embedding_size = self.text_model.transformer.wte.weight.shape[1]
+        if mapping_type == MappingType.MLP:
+            self.clip_project = MLP((prefix_size, (self.text_model_embedding_size * prefix_length) // 2,
+                                     self.text_model_embedding_size * prefix_length))
+        else:
+            self.clip_project = TransformerMapper(prefix_size, self.text_model_embedding_size, prefix_length,
+                                                                     clip_length, num_layers)
